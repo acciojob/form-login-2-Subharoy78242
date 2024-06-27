6625bc24c796c0ea73e01e31f459d9a464a08823
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-06-27 12:09:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const emailId = document.getElementById('emailId').value;
+    const firstName = document.getElementsByName('First Name')[0].value;
+    const lastName = document.getElementsByName('Last Name')[0].value;
+    const phoneNumber = document.getElementsByName('Phone Number')[0].value;
+    const emailId = document.getElementsByName('Email ID')[0].value;
 
     const alertMessage = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
     alert(alertMessage);
